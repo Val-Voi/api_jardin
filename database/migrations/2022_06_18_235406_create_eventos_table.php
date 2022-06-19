cced480d20d93f83631426f39735dd4bf63a93d1
Voifar <frann.lara@outlook.com>
2022-06-18 20:17:39 -0400

    migraciones

---
+++ database/migrations/2022_06_18_235406_create_eventos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->id();
+            $table->string('descripcion');
+            
+            $table->unsignedBigInteger('niños_id');
+            $table->foreign('niños_id')->references('id')->on('niños');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eventos');
+    }
+};
---
+++ database/migrations/2022_06_18_235551_create_ninos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('niños', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->date('fecha_nacimiento');
+            $table->string('rut');
+            $table->string('contacto_apoderado');
+
+            $table->unsignedBigInteger('niveles_id');
+            $table->foreign('niveles_id')->references('id')->on('niveles');
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('niños');
+    }
+};
